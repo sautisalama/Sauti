a484083881d36d4918ccf50054a080cdd709d394
oliv3r <oliversolomonwainaina@gmail.com>
2024-11-05 15:08:27 +0300

    fix: sender and recepient email bugs

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,13 @@
+import { getMaxListeners } from "events";
+
 export const REPORT_EMAIL_RECIPIENTS = [
-  { email: "admin@sautisalama.org" },
-  { email: "oliver@sautisalama.org" }
+    { email: "admin@sautisalama.org", name: "Sauti Salama Admin" },
+    { email: "oliver@sautisalama.org", name: "Oliver" },
+    { email: "malkia@sautisalama.org", name: "Malkia" },
+    { email: "oliverwai9na@gmail.com", name: "Oliver" }
 ];
 
 export const REPORT_EMAIL_SENDER = {
-  email: "hello@sautisalama.org",
-  name: "Sauti Salama Reports"
+    email: "report@sautisalama.org",
+    name: "Sauti Salama Reports"
 }; 
