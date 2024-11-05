6d76225b356832795b243dbc3a822ee440a56618
oliv3r <oliversolomonwainaina@gmail.com>
2024-11-05 14:01:44 +0300

    sending emails to admins once reports are made

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+export const REPORT_EMAIL_RECIPIENTS = [
+  { email: "admin@sautisalama.org" },
+  { email: "oliver@sautisalama.org" }
+];
+
+export const REPORT_EMAIL_SENDER = {
+  email: "hello@sautisalama.org",
+  name: "Sauti Salama Reports"
+}; 
