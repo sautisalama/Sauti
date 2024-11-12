7d544391166299683d4866df6867a6cf1dd016e0
oliv3r <oliversolomonwainaina@gmail.com>
2024-11-12 15:43:28 +0300

    chore: code cleanup

--- app/api/reports/route.ts
+++ app/api/reports/route.ts
@@ -1,28 +1,25 @@
 import { NextResponse } from "next/server";
-import { createClient } from "@/lib/supabase/supabase-server";
 import { MailtrapClient } from "mailtrap";
 import { REPORT_EMAIL_RECIPIENTS, REPORT_EMAIL_SENDER } from "@/lib/constants";
+import { createClient } from "@/utils/supabase/server";
 
 const TOKEN = process.env.MAILTRAP_TOKEN!;
 const client = new MailtrapClient({ token: TOKEN });
 
 export async function POST(request: Request) {
-  try {
-    const supabase = createClient();
-    const data = await request.json();
+	try {
+		const supabase = createClient();
+		const data = await request.json();
 
-    const { error } = await supabase
-      .from("reports")
-      .insert([data])
-      .select();
+		const { error } = await supabase.from("reports").insert([data]).select();
 
-    if (error) throw error;
+		if (error) throw error;
 
-    await client.send({
-      from: REPORT_EMAIL_SENDER,
-      to: REPORT_EMAIL_RECIPIENTS,
-      subject: `New Abuse Report: ${data.type_of_incident} (${data.urgency} urgency)`,
-      text: `
+		await client.send({
+			from: REPORT_EMAIL_SENDER,
+			to: REPORT_EMAIL_RECIPIENTS,
+			subject: `New Abuse Report: ${data.type_of_incident} (${data.urgency} urgency)`,
+			text: `
               New abuse report submitted:
               Type of Incident: ${data.type_of_incident}
               Urgency: ${data.urgency}
@@ -30,21 +27,21 @@ export async function POST(request: Request) {
               Reporter Information:
               Name: ${data.first_name}
               Email: ${data.email}
-              Phone: ${data.phone || 'Not provided'}
+              Phone: ${data.phone || "Not provided"}
               Preferred Contact Method: ${data.contact_preference}
               Consent to Share: ${data.consent}
 
               Submitted at: ${data.submission_timestamp}
                     `.trim(),
-      category: "Abuse Reports"
-    });
+			category: "Abuse Reports",
+		});
 
-    return NextResponse.json({ message: "Report submitted successfully" });
-  } catch (error) {
-    console.error("Error submitting report:", error);
-    return NextResponse.json(
-      { error: "Failed to submit report" },
-      { status: 500 }
-    );
-  }
-} 
+		return NextResponse.json({ message: "Report submitted successfully" });
+	} catch (error) {
+		console.error("Error submitting report:", error);
+		return NextResponse.json(
+			{ error: "Failed to submit report" },
+			{ status: 500 }
+		);
+	}
+}
