b6bcc8052e254a96a3869c57b8be35080032bbae
oliv3r <oliversolomonwainaina@gmail.com>
2024-11-12 15:00:19 +0300

    UPDATE - Added type safety using the Database type from your schema -  Simplified cookie handling with individual methods instead of bulk operations - Added useful utility functions for getting session and profile details

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,35 +1,65 @@
+import { Database, Tables } from "@/types/db-schema";
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export async function createClient() {
-	const cookieStore = await cookies();
+export function createClient() {
+	const cookieStore = cookies();
 
-	return createServerClient(
+	return createServerClient<Database>(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 		{
 			cookies: {
-				getAll() {
-					return cookieStore.getAll();
+				get(name: string) {
+					return cookieStore.get(name)?.value;
 				},
-				setAll(cookiesToSet) {
+				set(name: string, value: string, options: CookieOptions) {
 					try {
-						cookiesToSet.forEach(({ name, value, options }) =>
-							cookieStore.set(name, value, options)
-						);
-					} catch {
-						// The `setAll` method was called from a Server Component.
+						cookieStore.set({ name, value, ...options });
+					} catch (error) {
+						// The `set` method was called from a Server Component.
+						// This can be ignored if you have middleware refreshing
+						// user sessions.
+					}
+				},
+				remove(name: string, options: CookieOptions) {
+					try {
+						cookieStore.set({ name, value: "", ...options });
+					} catch (error) {
+						// The `delete` method was called from a Server Component.
 						// This can be ignored if you have middleware refreshing
 						// user sessions.
 					}
 				},
 			},
-			// Prevent user from being logged out randomly (They should manually log out or session gets terminated when browser is exited)
-			auth: {
-				autoRefreshToken: true,
-				persistSession: true,
-				detectSessionInUrl: true,
-			},
 		}
 	);
+}
+
+export async function getSession() {
+	const supabase = createClient();
+	const {
+		data: { session },
+	} = await supabase.auth.getSession();
+	return session;
+}
+
+export async function getProfileDetails() {
+	const supabase = createClient();
+	try {
+		const session = await getSession();
+		if (!session?.user?.id) return null;
+		const { data, error }: { data: Tables<"profiles"> | null; error: any } =
+			await supabase
+				.from("profiles")
+				.select("*")
+				.eq("id", session.user.id ?? "")
+				.single();
+
+		if (error) throw error;
+		return data;
+	} catch (error) {
+		console.error("get user profile error:", error);
+		return null;
+	}
 }
