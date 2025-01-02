c567d256fe6fbaa8e162f026fbb30724b5f729fb
oliv3r <oliversolomonwainaina@gmail.com>
2025-01-03 02:52:18 +0300

    fix; submitting reports for authenticated users

--- components/AuthenticatedReportAbuseForm.tsx
+++ components/AuthenticatedReportAbuseForm.tsx
@@ -84,8 +84,10 @@ export default function AuthenticatedReportAbuseForm({
 			return;
 		}
 
+		console.log(userProfile?.first_name);
+
 		const data = {
-			first_name: userProfile?.first_name || null,
+			first_name: userProfile?.first_name,
 			last_name: userProfile?.last_name || null,
 			user_id: userId,
 			phone: phone,
@@ -101,7 +103,7 @@ export default function AuthenticatedReportAbuseForm({
 		};
 
 		try {
-			const response = await fetch("/api/reports/submit", {
+			const response = await fetch("/api/reports", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
